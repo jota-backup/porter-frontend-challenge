6c45a6cfc5197f3aa41e1dfc744a9a44f79c2393
João Vítor Ferraz <jvkferraz@gmail.com>
2025-11-21 12:20:57 -0300

    feat: add apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./i18n.ts";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+	link: new HttpLink({ uri: import.meta.env.VITE_RICK_MORTY_GRAPHQL_API_URL }),
+	cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</StrictMode>,
 );
