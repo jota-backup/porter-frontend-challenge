7c3966829424043d850cbbba6eef22997769a98e
João Vítor Ferraz <jvkferraz@gmail.com>
2025-11-23 13:41:48 -0300

    feat: add get character details query

---
+++ src/graphql/queries/getCharacterDetails.ts
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+import { BASE_CHARACTER_FRAGMENT } from "../fragments/BaseCharacter";
+
+export const GET_CHARACTER_DETAILS = gql`
+	${BASE_CHARACTER_FRAGMENT}
+	query GetCharacterDetails($id: ID!) {
+		character(id: $id) {
+			...BaseCharacter
+			species
+			gender
+			type
+			location {
+				name
+			}
+			episode {
+				id
+			}
+			created
+		}
+	}
+`;
